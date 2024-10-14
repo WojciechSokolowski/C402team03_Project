f253717d8a0f490580270000622deaeb9e3c72f4
WojciechSokolowski <soko19@op.pl>
2024-10-14 21:26:17 +0200

    created the file for employee_viewer

---
+++ employee_viewer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+DB_USER="name"
+DB_PASS="pass"
+DB_NAME="name"
+DB_HOST="host"
+
+
+
+show_menu() {
+	clear
+	echo "Choose an option"
+	echo "1. Read all employees data"
+	echo "2. Add a new eployee"
+	echo "3. Edit employee"
+	echo "4. Remove employee"
+}
